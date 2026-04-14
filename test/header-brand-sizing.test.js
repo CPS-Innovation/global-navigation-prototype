9fdeb494b02b84f32c62e6ad872d09349e21ae3f
rob jenkins <robert.jenkins@cps.gov.uk>
2026-04-14 10:12:31 +0100

    test: reduce service name font size in header

--- test/header-brand-sizing.test.js
+++ test/header-brand-sizing.test.js
@@ -6,13 +6,23 @@ const sassPath = path.join(__dirname, '..', 'app', 'assets', 'sass', 'applicatio
 const sass = fs.readFileSync(sassPath, 'utf8')
 
 assert(
-  sass.includes('font-size: 30px;'),
-  'Expected header brand text to be 30px'
+  sass.includes('.app-cps-header__organisation {'),
+  'Expected organisation styling block to exist'
 )
 
 assert(
-  sass.includes('line-height: 1;'),
-  'Expected header brand line-height to be 1 so the blue banner height stays stable'
+  sass.includes('font-size: 30px;'),
+  'Expected CPS text to remain 30px'
+)
+
+assert(
+  sass.includes('.app-cps-header__service-name {'),
+  'Expected service name styling block to exist'
+)
+
+assert(
+  sass.includes('font-size: 24px;'),
+  'Expected service name text to be 24px'
 )
 
 console.log('header brand sizing checks passed')
