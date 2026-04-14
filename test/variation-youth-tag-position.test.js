215dab5fbf01312b3f961676f0c4e15916e192e9
rob jenkins <robert.jenkins@cps.gov.uk>
2026-04-14 15:02:40 +0100

    test: move variation youth tag below copy

---
+++ test/variation-youth-tag-position.test.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+const path = require('path')
+const assert = require('assert')
+
+const headerPath = path.join(__dirname, '..', 'app', 'views', 'includes', '_app-header-variation.html')
+const header = fs.readFileSync(headerPath, 'utf8')
+
+const ctlIndex = header.indexOf('id="app-ctl-summary"')
+const tagIndex = header.indexOf('>YOUTH<')
+
+assert(ctlIndex !== -1, 'Expected CTL summary placeholder')
+assert(tagIndex !== -1, 'Expected YOUTH tag')
+assert(tagIndex > ctlIndex, 'Expected YOUTH tag below the two text lines')
+
+console.log('variation youth tag position checks passed')
