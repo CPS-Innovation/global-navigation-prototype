b18c925596013a63a37895ae23d1a59bc7091cbc
rob jenkins <robert.jenkins@cps.gov.uk>
2026-04-14 15:32:04 +0100

    test: move variation tags inline with urn

--- test/variation-youth-tag-position.test.js
+++ test/variation-youth-tag-position.test.js
@@ -5,11 +5,11 @@ const assert = require('assert')
 const headerPath = path.join(__dirname, '..', 'app', 'views', 'includes', '_app-header-variation.html')
 const header = fs.readFileSync(headerPath, 'utf8')
 
-const ctlIndex = header.indexOf('id="app-ctl-summary"')
+const urnIndex = header.indexOf('{{ caseReference }}</span>')
 const tagIndex = header.indexOf('>Youth<')
 
-assert(ctlIndex !== -1, 'Expected CTL summary placeholder')
+assert(urnIndex !== -1, 'Expected URN span')
 assert(tagIndex !== -1, 'Expected YOUTH tag')
-assert(tagIndex > ctlIndex, 'Expected YOUTH tag below the two text lines')
+assert(tagIndex > urnIndex, 'Expected Youth tag inline after the URN')
 
 console.log('variation youth tag position checks passed')
