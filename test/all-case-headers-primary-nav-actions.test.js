11e37ae56f291706f6a0638fc66e88e9f789611b
rob jenkins <robert.jenkins@cps.gov.uk>
2026-06-09 14:59:51 +0100

    Add primary navigation action links

---
+++ test/all-case-headers-primary-nav-actions.test.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+const path = require('path')
+const assert = require('assert')
+
+const headerFiles = [
+  '_app-header.html',
+  '_app-header-variation.html',
+  '_app-header-variation-2.html',
+  '_app-header-variation-3.html'
+]
+
+headerFiles.forEach((file) => {
+  const header = fs.readFileSync(path.join(__dirname, '..', 'app', 'views', 'includes', file), 'utf8')
+
+  assert(
+    header.includes('<div class="govuk-width-container app-primary-navigation__actions">') &&
+      header.includes('<a class="app-primary-navigation__action" href="/not-yet-built">Report an issue</a>') &&
+      header.includes('<a class="app-primary-navigation__action" href="/not-yet-built">Give feedback</a>'),
+    `Expected ${file} to include right-aligned primary navigation action links`
+  )
+})
+
+console.log('all case headers primary navigation action checks passed')
---
+++ test/case-overview-primary-nav-actions.test.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+const path = require('path')
+const assert = require('assert')
+
+const headerPath = path.join(__dirname, '..', 'app', 'views', 'includes', '_app-header.html')
+const sassPath = path.join(__dirname, '..', 'app', 'assets', 'sass', 'application.scss')
+
+const header = fs.readFileSync(headerPath, 'utf8')
+const sass = fs.readFileSync(sassPath, 'utf8')
+
+assert(
+  header.includes('<div class="govuk-width-container app-primary-navigation__actions">') &&
+    header.includes('<a class="app-primary-navigation__action" href="/not-yet-built">Report an issue</a>') &&
+    header.includes('<a class="app-primary-navigation__action" href="/not-yet-built">Give feedback</a>'),
+  'Expected case overview primary navigation line to include right-aligned action links'
+)
+
+assert(
+  sass.includes('.app-primary-navigation {') && sass.includes('position: relative;'),
+  'Expected primary navigation to allow right-aligned action positioning'
+)
+
+assert(
+  sass.includes('.app-primary-navigation__actions {') &&
+    sass.includes('justify-content: flex-end;'),
+  'Expected primary navigation actions to be right aligned'
+)
+
+assert(
+  sass.includes('.app-primary-navigation__action {') &&
+    sass.includes('font-family: $govuk-font-family;'),
+  'Expected primary navigation actions to use the same font styling'
+)
+
+console.log('case overview primary navigation action checks passed')
